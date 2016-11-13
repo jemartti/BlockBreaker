a34d046fc0feb7bdb399237097c924426058da22
Jacob Marttinen <jacob@marttinen.ca>
2016-11-13 16:40:39 -0500

    Initial commit (up to prefab state)

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+
+	public void LoadLevel (string name)
+	{
+		Application.LoadLevel (name);
+	}
+	
+	public void LoadNextLevel (string name)
+	{
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest ()
+	{
+		Application.Quit ();
+	}
+}
