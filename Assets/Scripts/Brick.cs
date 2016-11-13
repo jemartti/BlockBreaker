34c8c67545c679f9a21c4ee916a3c470f76c89c6
Jacob Marttinen <jacob@marttinen.ca>
2016-11-13 16:56:20 -0500

    Destroying when hit

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -15,7 +15,9 @@ public class Brick : MonoBehaviour {
 	
 	void OnCollisionEnter2D (Collision2D collision) {
 		++timesHit;
-		SimulateWin();
+		if (timesHit >= maxHits) {
+			Destroy (gameObject);
+		}
 	}
 	
 	// TODO: Remove this method once we can actually win!
