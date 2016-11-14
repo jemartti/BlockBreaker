41e3ff4bd099fc43619a709a8a513b4f415b1a24
Jacob Marttinen <jacob@marttinen.ca>
2016-11-13 22:21:59 -0500

    Quick error log for missing sprite

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -46,8 +46,10 @@ public class Brick : MonoBehaviour
 	void LoadSprites ()
 	{
 		int spriteIndex = timesHit - 1;
-		if (hitSprites [spriteIndex]) {
+		if (hitSprites [spriteIndex] != null) {
 			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		} else {
+			Debug.LogError ("Brick sprite missing");
 		}
 	}
 }
