4d88281af7df73bd2ddfc84abff1cdf6336d54d7
Jacob Marttinen <jacob@marttinen.ca>
2016-11-13 21:37:40 -0500

    Automated play testing

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,12 +3,38 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour
 {
+	public bool autoPlay = false;
+	private Ball ball;
+	
+	void Start ()
+	{
+		ball = GameObject.FindObjectOfType<Ball> ();
+	}
+
 	void Update ()
+	{
+		if (!autoPlay) {
+			MoveWithMouse ();
+		} else {
+			AutoPlay ();
+		}
+	}
+	
+	void MoveWithMouse ()
 	{
 		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
 		
 		this.transform.position = new Vector3 (
 			Mathf.Clamp (mousePosInBlocks, 0.5f, 15.5f),
+			this.transform.position.y,
+			this.transform.position.z
+		);
+	}
+	
+	void AutoPlay ()
+	{
+		this.transform.position = new Vector3 (
+			ball.transform.position.x,
 			this.transform.position.y,
 			this.transform.position.z
 		);
