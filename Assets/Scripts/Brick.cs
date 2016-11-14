b5d1bcbd301cb7ff79c6badf121783220961715a
Jacob Marttinen <jacob@marttinen.ca>
2016-11-14 09:01:41 -0500

    Adding some particle systems to block breaks

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -6,6 +6,7 @@ public class Brick : MonoBehaviour
 	public static int breakableCount = 0;
 	public Sprite[] hitSprites;
 	public AudioClip crack;
+	public GameObject smoke;
 	private LevelManager levelManager;
 	private int timesHit;
 	private bool isBreakable;
@@ -25,7 +26,7 @@ public class Brick : MonoBehaviour
 	
 	void OnCollisionEnter2D (Collision2D collision)
 	{
-		AudioSource.PlayClipAtPoint (crack, transform.position, 0.25f);
+		AudioSource.PlayClipAtPoint (crack, gameObject.transform.position, 0.25f);
 		if (isBreakable) {
 			HandleHits ();
 		}
@@ -37,10 +38,21 @@ public class Brick : MonoBehaviour
 		if (timesHit >= hitSprites.Length + 1) {
 			--breakableCount;
 			levelManager.BrickDestroyed ();
+			PuffSmoke ();
 			Destroy (gameObject);
 		} else {
 			LoadSprites ();
 		}
+	}
+	
+	void PuffSmoke ()
+	{
+		GameObject puff = Object.Instantiate (
+			smoke,
+			gameObject.transform.position + new Vector3 (0f, 0f, -1f),
+			Quaternion.identity
+		) as GameObject;
+		puff.particleSystem.startColor = gameObject.GetComponent<SpriteRenderer> ().color;
 	}
 	
 	void LoadSprites ()
